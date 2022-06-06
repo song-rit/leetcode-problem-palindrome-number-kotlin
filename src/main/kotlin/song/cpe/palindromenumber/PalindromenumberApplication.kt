f7a3546baa942124457595e349f824cafce86562
song.cpe <song.cpe@gmail.com>
2022-06-06 16:36:10 +0700

    init project

---
+++ src/main/kotlin/song/cpe/palindromenumber/PalindromenumberApplication.kt
@@ -0,0 +1,11 @@
+package song.cpe.palindromenumber
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PalindromenumberApplication
+
+fun main(args: Array<String>) {
+	runApplication<PalindromenumberApplication>(*args)
+}
---
+++ src/test/kotlin/song/cpe/palindromenumber/PalindromenumberApplicationTests.kt
@@ -0,0 +1,13 @@
+package song.cpe.palindromenumber
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PalindromenumberApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
